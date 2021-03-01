179a6cd49e0a908843c6972417654d929ecbcc5c
Illia Terpylo <illiater99@gmail.com>
2021-03-01 06:35:52 +0200

    lab.cpp

---
+++ lab.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//comment
+
+int main()
+{
+    int count;
+    scanf("%d", &count);
+
+    printf("\n");
+    for (int i = 0; i < count; i++)
+    {
+        printf("%i\n", i);
+    }
+}
+
